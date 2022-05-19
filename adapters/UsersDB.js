02e50e9589309eea4c36398781381f6aedadc549
Dino <elvedingran@gmail.com>
2022-05-19 10:33:06 +0200

    refactor: code cleanup

--- adapters/UsersDB.js
+++ adapters/UsersDB.js
@@ -1,29 +1,11 @@
-
-const stockData = [{id: 1, title: "user1", age: 20 }, {id: 2, title: "user2", age: 33}, {id: 3, title: "user3", age: 56 } ]
-
-
 const AWS = require("aws-sdk");
-const express = require("express");
-const serverless = require("serverless-http");
-
-
-const app = express();
-
 
 const documentClient = new AWS.DynamoDB.DocumentClient();
 const USERS_TABLE = process.env.USERS_TABLE;
 
-app.use(express.json());
 
 const getUserValue = async (userId = "test") => {
-   /* try {
-        
-        return stockData.find(x=> x.id == stockID)
-    }
-    catch (err) {
-        console.log(err)
-        return err
-    } */
+
     let params = {
       TableName : USERS_TABLE,
       Key:{
--- adapters/getUsersRequest.js
+++ adapters/getUsersRequest.js
@@ -3,15 +3,17 @@ const HTTPHandler = require("../ports/HTTPHandler")
 
 async function getUsersRequest(userId){
     let res;
-
     try {   
         const userData = await HTTPHandler.retrieveUser(userId)
         
         res = {
             'statusCode': 200,
-            'body': JSON.stringify({
-                message: userData,
-            })
+            'body': JSON.stringify(userData)
+        }
+        if (res == null){
+            res = {
+                'statusCode': 404
+            }
         }
 
     } catch (err) {
